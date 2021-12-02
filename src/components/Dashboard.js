7151ac3276ab2d673a96117acdca7e5f190785db
Bruno Gomes <brunogomess7@gmail.com>
2021-12-02 12:36:54 -0300

    Pequenas alterações no layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -15,7 +15,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ListItems from "./ListItems";
 import RoutesHtseer from '../routes';
-
+import backgroundImg from '../images/BACKGROUND TOPO.png'
 
 function Copyright(props) {
   return (
@@ -88,40 +88,6 @@ function DashboardContent() {
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              HTSeer
-            </Typography>
-            {/* <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton> */}
-          </Toolbar>
-        </AppBar>
         <Drawer variant="permanent" open={open}>
           <Toolbar
             sx={{
@@ -131,8 +97,12 @@ function DashboardContent() {
               px: [1],
             }}
           >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
+            <IconButton 
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '14px'
+              }}>
+              <MenuIcon />
             </IconButton>
           </Toolbar>
           <Divider />
@@ -151,8 +121,10 @@ function DashboardContent() {
             overflow: 'auto',
           }}
         >
+            <img src={backgroundImg}/>    
           <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>    
+          
             {/* Menu com links para as rotas */}
             <RoutesHtseer />
             {/* Rodapé */}
